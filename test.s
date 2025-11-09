be62872fe82fc338b0c7721cbd691c5ae1a1fc5c
Seling11 <1527319985@qq.com>
2025-11-10 01:12:08 +0800

    解决test.s乱码问题

--- test.s
+++ test.s
@@ -1,13 +1,13 @@
-;初始化
+;Initialization
 START:
-MOVI R1, #10
-MOVI R2, #20
+MOVI R1, #10  ; Move 10 to R1
+MOVI R2, #20  ; Move 20 to R2
 ADD R3, R1, R2
 SUB R4, R3, R1
 LOOP:
 MUL R5, R4, R2
 JN 100
-;下面是存储指令
+;Store & Load
 ST R5, #200
 LD R6, #200
 CMP R6, R5, R1
